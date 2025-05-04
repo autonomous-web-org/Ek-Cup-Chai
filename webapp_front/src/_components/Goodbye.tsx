01597dfe37cbc000a51024f73267dc8b3a0377ee
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2025-05-04 10:33:45 +0530

    bug fix

--- webapp_front/src/_components/Goodbye.tsx
+++ webapp_front/src/_components/Goodbye.tsx
@@ -1,11 +1,10 @@
 const Goodbye = () => {
-    return (
-      <div className="min-h-screen flex flex-col justify-center items-center text-center text-6xl font-bold text-primary pb-[10%]">
-        👋 Thanks for checking out!
-        <p className="text-3xl mt-4">See you next time!</p>
-      </div>
-    );
-  };
-  
-  export default Goodbye;
-  
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center text-center text-6xl font-bold text-primary pb-[10%]">
+      👋 Thanks for checking out!
+      <p className="text-3xl mt-4">See you next time!</p>
+    </div>
+  );
+};
+
+export default Goodbye;
